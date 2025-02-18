fb1b4416ca4b943c5c20b845fbc179a106f58e5e
Nikolay Lyutov <nikolay.lyutov@ventionteams.com>
2025-02-18 14:43:14 +0200

    fix: update trpc ulr

--- app/_trpc/Provider.tsx
+++ app/_trpc/Provider.tsx
@@ -10,12 +10,14 @@ interface ProviderProps {
 }
 
 const Provider: React.FC<ProviderProps> = ({ children }) => {
+    const url = process.env.IS_PROD ? (process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : '') : 'http://localhost:3000/api/trpc'
+
     const [queryClient] = useState(() => new QueryClient({}));
     const [trpcClient] = useState(() =>
         trpc.createClient({
             links: [
                 httpBatchLink({
-                    url: 'http://localhost:3000/api/trpc'
+                    url
                 })
             ]
         })
